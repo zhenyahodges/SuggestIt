442db236826e9bcfbb18a6499877d910c66f9d2f
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-04-05 18:09:05 +0100

    cardItem details

--- suggestit/client/src/components/Catalog/Card/CardItem.js
+++ suggestit/client/src/components/Catalog/Card/CardItem.js
@@ -1,18 +1,17 @@
 import { Link, NavLink, useLoaderData } from 'react-router-dom';
-import { getCards } from '../../../utils/api';
+import { getCard, getCards } from '../../../utils/api';
 // import { SuggestionItem } from '../../AddSuggestion/SuggestionItem/SuggestionItem';
 // import { SuggestionItem } from '../../Details/AddSuggestion/SuggestionItem/SuggestionItem';
 
-export function loader({params}) {    
-    return getCards(params.cardId);
+export function loader({ params }) {
+    // console.log(params);
+    return getCard(params.cardId);
 }
 
 export default function CardItem() {
     const card = useLoaderData();
-    console.log(card.brand);
-    const { _ownerId, brand,_createdOn, _Id, suggestions}=card;
+    const [ownerId,brand,createdOn,suggestions] = card;
     // console.log(card);
-
 
     return (
         //  DETAILS vis for all
@@ -22,21 +21,22 @@ export default function CardItem() {
             {/*  LOADER show when fetching */}
             {/* <Loader/> */}
 
- {        // TODO:!!! Hide overflow!!!from Details?! or SHOW ONLY FIRST N SUGGS
+            {
+                // TODO:!!! Hide overflow!!!from Details?! or SHOW ONLY FIRST N SUGGS
 
-<article className='sugg-card details'>
-    <header className='card-header'>
-        <h5 className='brand'>{brand}</h5>
-    </header>
+                <article className='sugg-card details'>
+                    <header className='card-header'>
+                        <h5 className='brand'>{brand}</h5>
+                    </header>
 
-    <main className='card-main'>
-        <p>HERE GO THE SUGGESTIONS</p>
-        <ul className='sugg-list'>
-            {/* TODO: MAP SUGGESTIONS */}
-            {/* {suggestions && suggestions.map((s) => <SuggestionItem id={s._id} key={s._id} {...s} />)} */}
-            {/* {_id.suggestions.map(s=>(<SuggestionItem key={s._id} {...s}/>))}   */}
+                    <main className='card-main'>
+                        <p>HERE GO THE SUGGESTIONS</p>
+                        <ul className='sugg-list'>
+                            {/* TODO: MAP SUGGESTIONS */}
+                            {/* {suggestions && suggestions.map((s) => <SuggestionItem id={s._id} key={s._id} {...s} />)} */}
+                            {/* {_id.suggestions.map(s=>(<SuggestionItem key={s._id} {...s}/>))}   */}
 
-            {/* {card.suggestions &&
+                            {/* {card.suggestions &&
      Object.values(card.suggestions).map((x) => (
         <li key={x._id} className='sugg-item'>
              <div className='sugg-item-wrapper'>
@@ -44,53 +44,55 @@ export default function CardItem() {
                  <span className='author-nickname'>
                       {x.userName}
                 </span> */}
-            {/* SUGGESTION */}
-            {/* {x.suggestion} */}
-        </ul>
-    </main>
+                            {/* SUGGESTION */}
+                            {/* {x.suggestion} */}
+                        </ul>
+                    </main>
 
-    <footer className='card-footer suggs-card foot'>
-        <div className='card-footer-content'>
-            {/* <p className='card-footer-owner'>Owner</p> */}
-            <p className='card-footer-text'>
-                Thank you for your contributions!
-            </p>
+                    <footer className='card-footer suggs-card foot'>
+                        <div className='card-footer-content'>
+                            {/* <p className='card-footer-owner'>Owner</p> */}
+                            <p className='card-footer-text'>
+                                Thank you for your contributions!
+                            </p>
 
-            <div className='card-footer-links-wrapper'>            
+                            <div className='card-footer-links-wrapper'>
+                                {/* ADD-SUGGESTION LINK: visible for LOGGED (NOT OWNERS?) */}
+                                {/* {(isAuthorized && !isOwner)&& */}
+                                <Link
+                                    to='/addSuggestion'
+                                    className='add-sugg-link'
+                                >
+                                    Suggest
+                                </Link>
+                                {/* }  */}
 
-               {/* ADD-SUGGESTION LINK: visible for LOGGED (NOT OWNERS?) */}
-{/* {(isAuthorized && !isOwner)&& */}
-        <Link to='/addSuggestion' className='add-sugg-link'>
-        Suggest
-        </Link>
-     {/* }  */}
-
-                {/*------- LATER  */}
-                {/*  <a href="/" className="print details">Print</a> */}
-                {/*  <a href="/" className="print email details"
+                                {/*------- LATER  */}
+                                {/*  <a href="/" className="print details">Print</a> */}
+                                {/*  <a href="/" className="print email details"
                     >Email</a
                     > */}
 
-                {/*------- LATER  */}
-                {/*  VISIBLE FOR LOGGED OWNER ONLY */}
-                {/* <p className="countdown-text"><span className="count-end">20</span> days left</p> */}
+                                {/*------- LATER  */}
+                                {/*  VISIBLE FOR LOGGED OWNER ONLY */}
+                                {/* <p className="countdown-text"><span className="count-end">20</span> days left</p> */}
 
-                {/* if POLL ENDED  */}
-                {/* <p className="countdown-text">Poll ended</p> */}
+                                {/* if POLL ENDED  */}
+                                {/* <p className="countdown-text">Poll ended</p> */}
 
-                {/*  VISIBLE FOR OWNER IF NOT TIMED OUT */}
-                {/* { (isAuthorized && !isOwner)&&
+                                {/*  VISIBLE FOR OWNER IF NOT TIMED OUT */}
+                                {/* { (isAuthorized && !isOwner)&&
        <a href='/' className='card-details edit-card'>
             Edit
         </a>
         <a href='/' className='card-details delete-card'>
             Delete
         </a>} */}
-            </div>
-        </div>
-    </footer>
-</article>
-}
+                            </div>
+                        </div>
+                    </footer>
+                </article>
+            }
             {/* SUGGESTIONS */}
         </section>
     );
--- suggestit/client/src/utils/api.js
+++ suggestit/client/src/utils/api.js
@@ -17,22 +17,21 @@ export async function getCards(id) {
     // return (data);
 }
 
-// export async function getCard(id) {
-//     const url = id
-//         ? `${baseUrl}/jsonstore/cards/${id}`
-//         : `${baseUrl}/jsonstore/cards`;
-//     const res = await fetch(url);
-//     if (!res.ok) {
-//         throw new Error(`${res.status} - ${res.statusText}`);
-//     }
-//     if (res.statusCode === 204) {
-//         return console.log('empty');
-//     }
-//     const data = await res.json();
-//     // console.log(Object.values(data));
-//     return Object.values(data);
-//     // return (data);
-// }
+export async function getCard(id) {
+    const url =   `${baseUrl}/jsonstore/cards/${id}`;
+
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`${res.status} - ${res.statusText}`);
+    }
+    if (res.statusCode === 204) {
+        return console.log('empty');
+    }
+    const data = await res.json();
+    console.log(Object.values(data));
+    return Object.values(data);
+    // return (data);
+}
 
 export async function getSuggestions(id) {
     const url = `${baseUrl}/jsonstore/cards/${id}`;
