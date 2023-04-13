ec6051ba3bac05ac02815c2796787740989b46a2
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-04-13 22:03:07 +0100

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,12 +1,23 @@
 import { createContext, useState } from 'react';
+import { requireAuth } from '../utils/requireAuth';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [isAuth, setAuth] = useState(false);
-    const [isUser, setUser] = useState(false);
+    const [isAuth, setIsAuth] = useState(false);
+    const [isLogged, setIsLogged] = useState(false);
+
+    const {userId,token}=requireAuth();
+
+    userId? setIsLogged(true): setIsLogged(false);
+    token? setIsAuth(true): setIsAuth(false);
     
-    const contextValues='';
+    const contextValues={
+       isLogged,
+       setIsLogged,
+        isAuth,
+        setIsAuth
+    };
     
     return (
         <>
