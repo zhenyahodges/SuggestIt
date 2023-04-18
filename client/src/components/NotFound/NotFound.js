6492aba538454c99e03dd4fb67ad491a98cfd3c7
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-04-19 00:56:06 +0100

    Update NotFound.js

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -5,7 +5,16 @@ export const NotFound = () => {
         <section className='404 window'>
             <>
                 <h2 className='404 title'>Page Not Found</h2>
-                <Link to='/' style={{fontWeight:'bold', color:'#F79234',textShadow: '1px 1px 1px #132930'}}>BACK TO HOME PAGE</Link>
+                <Link
+                    to='/'
+                    style={{
+                        fontWeight: 'bold',
+                        color: '#F79234',
+                        textShadow: '1px 1px 1px #132930',
+                    }}
+                >
+                    BACK TO HOME PAGE
+                </Link>
             </>
         </section>
     );
