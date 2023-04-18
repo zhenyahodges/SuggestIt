88420162671aae6c275fda69110402aac29d3a82
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-04-19 00:58:10 +0100

    Update CardItem.js

--- client/src/components/Catalog/Card/CardItem.js
+++ client/src/components/Catalog/Card/CardItem.js
@@ -48,41 +48,41 @@ export default function CardItem() {
     // useEffect(()=>{
     //     // fetch
     // },[setLikes]);
-  
+
     const user = JSON.parse(localStorage.getItem('user'));
-    
+
     let userId;
     let token;
     if (user) {
         ({ token, userId } = user);
     }
-    
+
     let isOwner = false;
     let isAuthorized = false;
-    
+
     if (token) {
         isAuthorized = true;
     }
     if (ownerId === userId) {
         isOwner = true;
     }
-    
+
     const onDelete = async () => {
         if (window.confirm('Are you sure you want to delete?')) {
             await onDeleteCard(cardId, token);
 
             navigate(`/users/${userId}`);
         }
-    };    
-    
+    };
+
     function onPrint(e) {
         e.preventDefault();
         window.print();
         return false;
     }
 
-    const minutes=100000;
-    const timePassed=new Date()-new Date(createdOn)>minutes;
+    const minutes = 100000;
+    const timePassed = new Date() - new Date(createdOn) > minutes;
 
     // useEffect(() => {
     //     likes && setLikesSugg(likes);
@@ -92,23 +92,22 @@ export default function CardItem() {
     //     const res=await getLikes(suggestionId,token);
     //     console.log('LIKESRES--'+res);
     // }
-    
-//     async function increase(suggestionId,token){ 
-//         console.log('sid--'+suggestionId,'tok--'+token);   
-//     //     console.log(suggestionId,token);
-//         await postLikes(suggestionId,token);
-     
 
-//     //     const res=await getLikes(suggestionId);
-//     setLikesSugg(prevCount=>prevCount+1);
-//  }
+    //     async function increase(suggestionId,token){
+    //         console.log('sid--'+suggestionId,'tok--'+token);
+    //     //     console.log(suggestionId,token);
+    //         await postLikes(suggestionId,token);
 
-//   async function decrease(){
-//     setLikesSugg(prevCount=>prevCount-1);
-//  }
+    //     //     const res=await getLikes(suggestionId);
+    //     setLikesSugg(prevCount=>prevCount+1);
+    //  }
 
- return (
-     <section className='details-view container'>
+    //   async function decrease(){
+    //     setLikesSugg(prevCount=>prevCount-1);
+    //  }
+
+    return (
+        <section className='details-view container'>
             <h2>Details</h2>
             {
                 <>
@@ -118,7 +117,6 @@ export default function CardItem() {
                         </header>
 
                         <main className='card-main'>
-                           
                             <ul className='sugg-list'>
                                 {/* SUGGESTIONS */}
                                 {suggs &&
@@ -143,26 +141,44 @@ export default function CardItem() {
                                                             {suggestion}
 
                                                             {/* <!--IF OWNER & NOT TIMED OUT --> */}
-                                                           {(_ownerId===userId && !(new Date()-new Date(_createdOn)>100000))&&
-                                                            <span className='user-sug-list'>
-                                                                <Link
-                                                                    to={`/suggestions/${_id}`}
-                                                                    className='edit-user-sugged link'
-                                                                >
-                                                                    Edit
-                                                                </Link>
-                                                                <Link
-                                                                    onClick={() => {
-                                                                        if (window.confirm('Are you sure you want to delete?')) {
-                                                                            onDeleteSuggestion(_id, token);
-                                                                            navigate(`/cards/${cardId}`);
-                                                                        }
-                                                                    }}
-                                                                    className='delete-user-sugged link'
-                                                                >
-                                                                    Delete
-                                                                </Link>
-                                                            </span>}
+                                                            {_ownerId ===
+                                                                userId &&
+                                                                !(
+                                                                    new Date() -
+                                                                        new Date(
+                                                                            _createdOn
+                                                                        ) >
+                                                                    100000
+                                                                ) && (
+                                                                    <span className='user-sug-list'>
+                                                                        <Link
+                                                                            to={`/suggestions/${_id}`}
+                                                                            className='edit-user-sugged link'
+                                                                        >
+                                                                            Edit
+                                                                        </Link>
+                                                                        <Link
+                                                                            onClick={() => {
+                                                                                if (
+                                                                                    window.confirm(
+                                                                                        'Are you sure you want to delete?'
+                                                                                    )
+                                                                                ) {
+                                                                                    onDeleteSuggestion(
+                                                                                        _id,
+                                                                                        token
+                                                                                    );
+                                                                                    navigate(
+                                                                                        `/cards/${cardId}`
+                                                                                    );
+                                                                                }
+                                                                            }}
+                                                                            className='delete-user-sugged link'
+                                                                        >
+                                                                            Delete
+                                                                        </Link>
+                                                                    </span>
+                                                                )}
                                                         </p>
 
                                                         <p className='sugg-ranking'>
@@ -174,16 +190,18 @@ export default function CardItem() {
 
                                                             {/* <!-- LIKE DISABLED FOR GUESTS & OWNERS -->
                                                             <!--===!? LIKE LIMITED voting!?=== --> */}
-                                                            <button className='sugg-like-link' 
-                                                            // onClick={()=>increase(_id,token)}
+                                                            <button
+                                                                className='sugg-like-link'
+                                                                // onClick={()=>increase(_id,token)}
                                                             >
                                                                 {/* if voted down=>vote up */}
                                                                 <i className='like fa-solid fa-circle-up'></i>
                                                                 {/* if voted up=>vote down */}
                                                                 {/* <i className='fa-solid fa-circle-down'></i> */}
                                                             </button>
-                                                            <button className='sugg-like-link' 
-                                                        //    onClick={()=>decrease(_id,token)}
+                                                            <button
+                                                                className='sugg-like-link'
+                                                                //    onClick={()=>decrease(_id,token)}
                                                             >
                                                                 <i className='fa-solid fa-circle-down'></i>
                                                             </button>
@@ -242,12 +260,11 @@ export default function CardItem() {
                                     {/* <p className="countdown-text">Poll ended</p> */}
 
                                     {/* EDIT/DELETE VISIBLE FOR OWNER IF NOT TIMED OUT */}
-                                    {((isAuthorized && isOwner)&&(!timePassed)) && (
+                                    {isAuthorized && isOwner && !timePassed && (
                                         <>
                                             <Link
                                                 to={`/cards/${cardId}/edit`}
                                                 className='btn-sm card-details edit-card'
-                                              
                                                 disabled={
                                                     navigation.state ===
                                                     'loading'
