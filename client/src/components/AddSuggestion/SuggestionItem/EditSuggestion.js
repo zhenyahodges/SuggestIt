2c446ad9d5286488739b6de4e76b4391581f98c3
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-04-19 00:46:57 +0100

    Update EditSuggestion.js

--- client/src/components/AddSuggestion/SuggestionItem/EditSuggestion.js
+++ client/src/components/AddSuggestion/SuggestionItem/EditSuggestion.js
@@ -1,39 +1,28 @@
 import { Form, redirect, useLoaderData, useNavigation } from 'react-router-dom';
 import { requireAuth } from '../../../utils/requireAuth';
-import {
-    addSuggestion,
-    editSuggestion,
-    getCards,
-    getOneSuggestions,
-    onEditSuggestion,
-} from '../../../utils/service';
-import { useState } from 'react';
+import { getOneSuggestions, onEditSuggestion } from '../../../utils/service';
+
 let cardIds;
 
 export async function loader({ request, params }) {
-    const { userId, token } = await requireAuth(request);
-    // console.log('params=='+Object.entries(params));
+    const { token } = await requireAuth(request);
     const suggestionId = params.suggestionId;
     const res = await getOneSuggestions(suggestionId, token);
-    // const {_ownerId,suggestion,cardId,_createdOn,_id,author}=res[0];
-    // console.log('resedit=='+_ownerId,suggestion,cardId,_createdOn,_id);
-    // console.log('resedit=='+Object.entries(res));;
     return res;
 }
 
 export async function action({ request, params }) {
     if (window.confirm('Are you sure you want to submit?')) {
-        const { userId, token } = await requireAuth();
-        console.log(userId, token);
+        const { token } = await requireAuth();
         const formData = await request.formData();
         const suggestion = formData.get('suggestion');
         const suggestionId = params.suggestionId;
-        const cardId=cardIds;
+        const cardId = cardIds;
 
         try {
             if (token) {
-                await onEditSuggestion(token,cardId, suggestion, suggestionId);
-                return redirect(`/cards`);
+                await onEditSuggestion(token, cardId, suggestion, suggestionId);
+                return redirect('/cards');
             } else {
                 redirect('login');
             }
@@ -46,9 +35,18 @@ export async function action({ request, params }) {
 export default function EditSuggestion() {
     const navigation = useNavigation();
     const res = useLoaderData();
-    const { _ownerId, suggestion, cardId, _createdOn,_updatedOn, _id, author } = res[0];
-    // const updatedOn=res._updatedOn;
-cardIds=cardId;
+    const {
+        _ownerId,
+        suggestion,
+        cardId,
+        _createdOn,
+        _updatedOn,
+        _id,
+        author,
+    } = res[0];
+
+    cardIds = cardId;
+    
     return (
         <section className='add-sugg form-wrapper'>
             <Form method='post' id='add-form' className='add-sugg form'>
