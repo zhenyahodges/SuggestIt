19028bc2ca40f5266670380fc3cc16fdf4d77513
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-04-19 01:11:21 +0100

    Update CurrentUserContext.js

--- client/src/context/CurrentUserContext.js
+++ client/src/context/CurrentUserContext.js
@@ -12,7 +12,6 @@ export const CurrentUserProvider = ({ children }) => {
     const userData = JSON.parse(localStorage.getItem('user'));
 
     useEffect(() => {
-        // console.log('USER-SURR---' + userData);
         if (userData) {
             let email = userData.email;
             email ? setWhoIsLooking(email) : setWhoIsLooking('Guest');
