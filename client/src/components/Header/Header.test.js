e7e6a0d8a218ed16115e3b78a326b2b42bcb954d
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-11-18 12:34:20 +0000

    testing

--- client/src/components/Header/Header.test.js
+++ client/src/components/Header/Header.test.js
@@ -1,10 +1,41 @@
-// import { render, screen } from '@testing-library/react';
-// import Header from './Header';
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
 
-// describe('Header', () => {
-//     it('renders Header component', () => {
-//         render(<Header />);
-//         const headingElement=screen.getByRole('heading', {level: 1});
-//         expect(headingElement).toBeInTheDocument();        
-//     });
-// });
+import React, { isValidElement } from 'react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import { CurrentUserProvider } from '../../context/CurrentUserContext';
+import { LoggedProvider } from '../../context/LoggedContext';
+
+export function renderWithRouter(children, routes = []) {
+    const options = isValidElement(children)
+        ? { element: children, path: '/' }
+        : children;
+
+    const router = createMemoryRouter([{ ...options }, ...routes], {
+        initialEntries: [options.path],
+        initialIndex: 1,
+    });
+
+    return render(
+        <CurrentUserProvider>
+            <LoggedProvider>
+                <RouterProvider router={router} />
+            </LoggedProvider>
+        </CurrentUserProvider>
+    );
+}
+
+describe('Header', () => {
+    it('renders Header', async () => {
+        renderWithRouter(<Header />);
+        expect(screen.getByText('Catalog')).toBeInTheDocument();        
+    });
+    it('shows links', async () => {
+        renderWithRouter(<Header />);
+        expect(screen.getAllByRole('link')).toHaveLength(6);        
+    });
+    // it('does not show links', async () => {
+    //     renderWithRouter(<Header />);
+    //     expect(screen.getAllByRole('link')).toHaveLength(0);        
+    // });
+});
