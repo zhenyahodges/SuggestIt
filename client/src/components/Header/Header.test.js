4aedfa919e31e6da17c728a57b49f1d601f3ec92
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-11-18 00:24:35 +0000

    Create Header.test.js

---
+++ client/src/components/Header/Header.test.js
@@ -0,0 +1,10 @@
+// import { render, screen } from '@testing-library/react';
+// import Header from './Header';
+
+// describe('Header', () => {
+//     it('renders Header component', () => {
+//         render(<Header />);
+//         const headingElement=screen.getByRole('heading', {level: 1});
+//         expect(headingElement).toBeInTheDocument();        
+//     });
+// });
