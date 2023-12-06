cb56addb44d969b8e098ebbec375754961c5bcab
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-06 10:51:07 +0000

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -2,22 +2,21 @@ import { makeRequest } from './makeRequest';
 
 const baseUrl = 'http://localhost:3030/users';
 
-
 export async function loginUser(creds) {
-    return makeRequest(baseUrl,'/login', 'post', creds);
+    return makeRequest(baseUrl, '/login', 'post', creds);
 }
 export async function registerUser(creds) {
-    return makeRequest(baseUrl,'/register', 'post', creds);
+    return makeRequest(baseUrl, '/register', 'post', creds);
 }
 
 export async function logoutUser(token) {
-    return makeRequest(baseUrl,'/logout', 'get', null, {        
+    return makeRequest(baseUrl, '/logout', 'get', null, {
         'X-Authorization': token,
     });
 }
 
 export async function getUserInfo(token) {
-    return makeRequest(baseUrl,'/me', 'get', null, {        
+    return makeRequest(baseUrl, '/me', 'get', null, {
         'X-Authorization': token,
     });
 }
