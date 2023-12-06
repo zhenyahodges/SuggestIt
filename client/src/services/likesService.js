f47b193c118a774b37ddc4d0f9ab4387ea336eab
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-06 16:13:27 +0000

    Update likesService.js

--- client/src/services/likesService.js
+++ client/src/services/likesService.js
@@ -4,7 +4,7 @@ const baseUrl = 'http://localhost:3030/data/likes';
 
 export async function postLike(suggestionId, token, userId) {
     const info = { suggestionId, userId };
-    const data = await makeRequest(baseUrl,'','POST', info, {
+    const data = await makeRequest(baseUrl, '', 'POST', info, {
         'X-Authorization': token,
     });
     return data;
@@ -13,42 +13,25 @@ export async function postLike(suggestionId, token, userId) {
 export async function getOneLike(suggId, userId) {
     const searchQuery = encodeURIComponent(`suggestionId="${suggId}"`);
     const url = `${baseUrl}?where=${searchQuery}`;
-    const data = await makeRequest(url,'', 'GET');
+    const data = await makeRequest(url, '', 'GET', null);
 
     if (data) {
-      const likeId = data.find((item) => item.userId === userId)?._id;
-      return likeId;
+        const likeId = data.find((item) => item.userId === userId)?._id;
+        return likeId;
     }
     return null;
 }
 
 export async function deleteLike(likeId, token) {
-    const res = await fetch(`${baseUrl}/${likeId}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Authorization': token,
-        },
+    const data = await makeRequest(baseUrl, `/${likeId}`, 'DELETE', null, {
+        'X-Authorization': token,
     });
-    if (!res.ok) {
-        throw new Error(`${res.status} - ${res.statusText}`);
-    }
-    const data = await res.json();
     return data;
 }
 
 export async function getSuggestionLikesCount(suggestionId) {
     const searchQuery = encodeURIComponent(`suggestionId="${suggestionId}"`);
     const url = `${baseUrl}?where=${searchQuery}&count`;
-    const res = await fetch(url, {
-        metod: 'GET',
-    });
-    if (res.status === 404) {
-        return null;
-    }
-    if (!res.ok) {
-        throw new Error(`${res.status} - ${res.statusText}`);
-    }
-    const data = await res.json();
+    const data = await makeRequest(url,'', 'GET',null);
     return data;
 }
